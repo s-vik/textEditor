078643f053aed12b85dc591d7e7f36cdb52d3aa8
Viktor Sysyuk <s.vik.json@gmail.com>
2020-10-19 13:07:04 +0300

    Updates

---
+++ precache-manifest.eaa154d48a341a08eec6a0c956a7c424.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd429edc88130eb25bba92f0cb6d5745",
+    "url": "/text-editor/index.html"
+  },
+  {
+    "revision": "883e49d06e2aa547cd57",
+    "url": "/text-editor/static/css/main.b45e8ae9.chunk.css"
+  },
+  {
+    "revision": "f2e471563ec5340f1d53",
+    "url": "/text-editor/static/js/2.c8f9089c.chunk.js"
+  },
+  {
+    "revision": "607f4dba556022527177500adaa9aa68",
+    "url": "/text-editor/static/js/2.c8f9089c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "883e49d06e2aa547cd57",
+    "url": "/text-editor/static/js/main.474b96ee.chunk.js"
+  },
+  {
+    "revision": "a9180d7b928b86a79e4b",
+    "url": "/text-editor/static/js/runtime-main.24f782e7.js"
+  },
+  {
+    "revision": "7145a7021531d2f7870350b6fe6cf0b7",
+    "url": "/text-editor/static/media/logo.7145a702.svg"
+  }
+]);
